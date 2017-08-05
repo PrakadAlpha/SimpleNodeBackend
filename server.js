846c056e3d1f77f8b95a1e9035bb0ef41ebd0e6a
PrakadAlpha <prakadheshwarancbe@gmail.com>
2017-08-05 11:27:26 +0530

    Adding all

---
+++ server.js
@@ -0,0 +1,29 @@
+const express = require('express');
+
+const bodyParser = require('body-parser');
+var router = require('./router/api');
+const url = require('url');
+var db = require('./db');
+var app = express();
+//create Server
+
+
+
+app.use('/api',router);
+
+
+
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+
+//createConnection
+
+
+
+
+
+app.listen('3031', () => {
+  console.log('Server started..');
+});
